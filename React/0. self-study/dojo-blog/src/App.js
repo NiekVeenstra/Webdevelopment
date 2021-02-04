1faed86b4f674ffb8f22f7b745529416dbce1d9c
Niek <n.veenstra02@gmail.com>
2021-02-04 17:00:55 +0100

    react self studie

--- React/0. self-study/dojo-blog/src/App.js
+++ React/0. self-study/dojo-blog/src/App.js
@@ -1,17 +1,29 @@
-import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-  const title = "welcome";
-  const likes = 50;
-  // const person = { name: "yoshi", age: 30 };
+  // const title = "welcome to my React testing page!";
+  // const likes = 50;
+  // // const person = { name: "yoshi", age: 30 };
+  // const googleLink = "http://www.google.com";
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>liked {likes} times</p>
+        <Home />
+
+        {/* <h1>{title}</h1>
+        <p>liked {likes} times</p> */}
 
         {/* <p>{person}</p> */}
+
+        {/* <p>{10}</p>
+        <p>{"ten"}</p>
+        <p>{[1, 2, 3, 4, 5]}</p>
+        <p>{Math.random() * 10}</p>
+
+        <a href={googleLink}>Google</a> */}
       </div>
     </div>
   );
---
+++ React/0. self-study/dojo-blog/src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>The React Test Page</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New Post</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
