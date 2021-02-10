98824e0062d4a24d60e5ae8bdc30246c7bf9b0d7
Niek <n.veenstra02@gmail.com>
2021-02-10 21:59:07 +0100

    react self studie

--- React/0. self-study/reactCourseDev/chapter 3/react-music-player/src/App.js
+++ React/0. self-study/reactCourseDev/chapter 3/react-music-player/src/App.js
@@ -4,6 +4,7 @@ import "./styles/app.scss";
 // Adding Components
 import Player from "./components/Player";
 import Song from "./components/Song";
+import Library from "./components/Library";
 // Import Util
 import data from "./util";
 
@@ -11,10 +12,12 @@ function App() {
   // State
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
       <Song currentSong={currentSong} />
-      <Player />
+      <Player isPlaying={isPlaying} setIsPlaying={setIsPlaying} currentSong={currentSong} />
+      <Library songs={songs}/>
     </div>
   );
 }
