70f71849891da1b0a904d4141888aed877d2ac43
Niek <n.veenstra02@gmail.com>
2021-02-13 21:32:41 +0100

    react self studie

--- React/0. self-study/reactCourseDev/chapter 3/react-music-player/src/components/Player.js
+++ React/0. self-study/reactCourseDev/chapter 3/react-music-player/src/components/Player.js
@@ -1,10 +1,8 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay, faAngleLeft, faAngleRight, faPause } from "@fortawesome/free-solid-svg-icons";
 
-const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
-  // Ref
-  const audioRef = useRef(null);
+const Player = ({ songInfo, setSongInfo, audioRef, currentSong, isPlaying, setIsPlaying }) => {
   // Event Handlers
   const playSongHandler = () => {
     if (isPlaying) {
@@ -15,11 +13,6 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
       setIsPlaying(!isPlaying);
     }
   };
-  const timeUpdateHandler = (e) => {
-    const current = e.target.currentTime;
-    const duration = e.target.duration;
-    setSongInfo({ ...songInfo, currentTime: current, duration });
-  };
   const getTime = (time) => {
     return Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2);
   };
@@ -28,10 +21,6 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
     setSongInfo({ ...songInfo, currentTime: e.target.value });
   };
   // State
-  const [songInfo, setSongInfo] = useState({
-    currentTime: 0,
-    duration: 0,
-  });
   return (
     <div className="player">
       <div className="time-control">
@@ -55,12 +44,6 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
         />
         <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} />
       </div>
-      <audio
-        onTimeUpdate={timeUpdateHandler}
-        onLoadedMetadata={timeUpdateHandler}
-        ref={audioRef}
-        src={currentSong.audio}
-      ></audio>
     </div>
   );
 };
