36c67496f5ce823e3ef21c00b5b75c135684dfac
Niek <n.veenstra02@gmail.com>
2021-02-14 20:58:03 +0100

    react self studie finished music player

--- React/0. self-study/reactCourseDev/chapter 3/react-music-player/src/components/LibrarySong.js
+++ React/0. self-study/reactCourseDev/chapter 3/react-music-player/src/components/LibrarySong.js
@@ -1,21 +1,30 @@
 import React from "react";
 
-const LibrarySong = ({ song, songs, setCurrentSong, id, audioRef, isPlaying }) => {
-  const songSelectHandler = () => {
+const LibrarySong = ({ song, songs, setCurrentSong, id, audioRef, isPlaying, setSongs }) => {
+  const songSelectHandler = async () => {
     const selectedSong = songs.filter((state) => state.id === id);
-    setCurrentSong(selectedSong[0]);
-    audioRef.current.play();
-    if (isPlaying) {
-      const playPromise = audioRef.current.play();
-      if (playPromise !== undefined) {
-        playPromise.then((audio) => {
-          audioRef.current.play();
-        });
+    await setCurrentSong(selectedSong[0]);
+    // add active state
+    const newSongs = songs.map((song) => {
+      if (song.id === id) {
+        return {
+          ...song,
+          active: true,
+        };
+      } else {
+        return {
+          ...song,
+          active: false,
+        };
       }
-    }
+    });
+    setSongs(newSongs);
+    // check if song is playing
+    // audioRef.current.play();
+    if (isPlaying) audioRef.current.play();
   };
   return (
-    <div onClick={songSelectHandler} className="library-song">
+    <div onClick={songSelectHandler} className={`library-song ${song.active ? "selected" : ""}`}>
       <img alt={song.name} src={song.cover}></img>
       <div className="song-description">
         <h3>{song.name}</h3>
