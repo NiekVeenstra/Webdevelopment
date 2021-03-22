7618ee1d0ddc4be9c3baf2b1eb1cb8abdda55ad5
Niek <n.veenstra02@gmail.com>
2021-03-22 16:04:53 +0100

    ..broken

--- React/4.firebase/firebase/src/components/App.js
+++ React/4.firebase/firebase/src/components/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 
+import Signup from "./Signup";
+
+import { Container } from "react-bootstrap";
+
+import { AuthProvider } from "../context/AuthContext";
+
 const App = () => {
   return (
-    <div>
-      <h1>hello me</h1>
-    </div>
+    <AuthProvider>
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Signup />
+        </div>
+      </Container>
+    </AuthProvider>
   );
 };
 
