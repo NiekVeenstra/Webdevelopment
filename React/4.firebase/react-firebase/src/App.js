7ae1b9438d05f43dca6a2d4b5d110beb3e38e7f3
Niek <n.veenstra02@gmail.com>
2021-03-25 17:06:08 +0100

    start firebase

---
+++ React/4.firebase/react-firebase/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import { AuthProvider } from "./components/Auth";
+import PrivateRoute from "./components/PrivateRoute";
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <Router>
+        <div>
+          <PrivateRoute exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+        </div>
+      </Router>
+    </AuthProvider>
+  );
+};
+
+export default App;
