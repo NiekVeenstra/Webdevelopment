684d1feb4a4760ec6e938327e91c162fe820757e
Niek <n.veenstra02@gmail.com>
2021-03-27 18:17:07 +0100

    ..

--- React/4.firebase/react-firebase/src/components/Crypto.js
+++ React/4.firebase/react-firebase/src/components/Crypto.js
@@ -5,21 +5,23 @@ const Crypto = ({ name, price, symbol, marketcap, volume, image, priceChange }) 
     <div className="crypto-container">
       <div className="row">
         <div className="row__crypto">
-          <img src={image} alt="crypto" className="row__crypto__img"/>
-          <h1 className="row__crypto__name">{name}</h1>
-          <p className="row__crypto__crypto-symbol">{symbol}</p>
+          <img src={image} alt="crypto" className="row__crypto__img" />
+          <div className="row__crypto__name-container">
+            <h1 className="row__crypto__name">{name}</h1>
+            <p className="row__crypto__crypto-symbol">({symbol})</p>
+          </div>
         </div>
-        <div className="crypto-data">
-          <p className="crypto-price">&#x20AC;{price}</p>
-          <p className="crypto-volume">&#x20AC;{volume.toLocaleString()}</p>
+        <div className="row__data">
+          <p className="row__data__price">&#x20AC;{price}</p>
+          {/* <p className="crypto-volume">&#x20AC;{volume.toLocaleString()}</p> */}
 
           {priceChange < 0 ? (
-            <p className="crypto-percent red">{priceChange.toFixed(2)}%</p>
+            <p className="row__data__percent red">{priceChange.toFixed(2)}%</p>
           ) : (
-            <p className="crypto-percent green">{priceChange.toFixed(2)}%</p>
+            <p className="row__data__percent green">{priceChange.toFixed(2)}%</p>
           )}
 
-          <p className="crypto-marketcap">Cap: &#x20AC;{marketcap.toLocaleString()}</p>
+          {/* <p className="crypto-marketcap">Cap: &#x20AC;{marketcap.toLocaleString()}</p> */}
         </div>
       </div>
     </div>
