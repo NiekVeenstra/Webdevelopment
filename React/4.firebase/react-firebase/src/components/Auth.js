cc3a8ed5cebb3207b927df945327055312f3d2c6
Niek <n.veenstra02@gmail.com>
2021-03-29 11:45:34 +0200

    ..

--- React/4.firebase/react-firebase/src/components/Auth.js
+++ React/4.firebase/react-firebase/src/components/Auth.js
@@ -1,5 +1,41 @@
+// import React, { useEffect, useState } from "react";
+// import app from "../base";
+
+// const auth = app.auth()
+
+// export const AuthContext = React.createContext();
+
+// export const AuthProvider = ({ children }) => {
+//   const [currentUser, setCurrentUser] = useState(null);
+//   const [pending, setPending] = useState(true);
+
+//   useEffect(() => {
+//     app.auth().onAuthStateChanged((user) => {
+//       setCurrentUser(user);
+//       setPending(false);
+//     });
+//   }, []);
+
+//   if (pending) {
+//     return <>Loading...</>;
+//   }
+
+//   return (
+//     <AuthContext.Provider
+//       value={{
+//         currentUser,
+//       }}
+//     >
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// };
+
+
 import React, { useEffect, useState } from "react";
 import app from "../base";
+
+// const auth = app.auth()
 
 export const AuthContext = React.createContext();
 
--- React/4.firebase/react-firebase/src/pages/SignUp.js
+++ React/4.firebase/react-firebase/src/pages/SignUp.js
@@ -1,14 +1,25 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import { withRouter } from "react-router";
 import app from "../base";
 
 const SignUp = ({ history }) => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+  const [error, setError] = useState("");
   const handleSignUp = useCallback(
     async (event) => {
       event.preventDefault();
-      const { email, password } = event.target.elements;
+      // const { email, password } = event.target.elements;
+
+      if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+        return setError("Passwords do not match");
+      }
+
       try {
-        await app.auth().createUserWithEmailAndPassword(email.value, password.value);
+        await app
+          .auth()
+          .createUserWithEmailAndPassword(emailRef.current.value, passwordRef.current.value);
         history.push("/");
       } catch (error) {
         alert(error);
@@ -20,14 +31,29 @@ const SignUp = ({ history }) => {
   return (
     <div>
       <h1>Sign up</h1>
+      {error && (
+        <div className="text-red" variant="danger">
+          {error}
+        </div>
+      )}
       <form onSubmit={handleSignUp}>
         <label>
-          Email
-          <input name="email" type="email" placeholder="Email" />
+          Email:
+          <input name="email" type="email" ref={emailRef} placeholder="Email" required />
         </label>
         <label>
-          Password
-          <input name="password" type="password" placeholder="Password" />
+          Password:
+          <input name="password" type="password" ref={passwordRef} placeholder="Password" />
+        </label>
+        <label>
+          Password Confirmation:
+          <input
+            name="password"
+            type="password"
+            ref={passwordConfirmRef}
+            placeholder="Password"
+            required
+          />
         </label>
         <button type="submit">Sign Up</button>
       </form>
