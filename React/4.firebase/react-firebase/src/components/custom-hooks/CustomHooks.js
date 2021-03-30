e05930d63492cb6caa612b21128271c39ad57357
Niek <n.veenstra02@gmail.com>
2021-03-30 20:09:57 +0200

    creating custom hooks folder

---
+++ React/4.firebase/react-firebase/src/components/custom-hooks/CustomHooks.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react'
+
+export const CustomHooks = () => {
+    const [open, setOpen] = useState(false);
+    return (
+        <div>
+            
+        </div>
+    )
+}
