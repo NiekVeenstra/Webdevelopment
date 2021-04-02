65d1eea93ef0d84938356292c0e465d89ccfe142
Niek <n.veenstra02@gmail.com>
2021-04-02 12:01:51 +0200

    added multiple days to the chart

--- React/4.firebase/react-firebase/src/components/crypto-details-component/HistoryChart.js
+++ React/4.firebase/react-firebase/src/components/crypto-details-component/HistoryChart.js
@@ -1,26 +1,35 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Line, defaults } from "react-chartjs-2";
 import Chartjs from "chart.js";
 import { historyOptions } from "../../chartConfigs/chartConfigs";
 
-const HistoryChart = () => {
+const HistoryChart = ({ data }) => {
   const chartRef = useRef();
+  const { day, week, year, detail } = data;
+  const [timeFormat, setTimeFormat] = useState("24h");
+
+  const determineTimeFormat = () => {
+    switch (timeFormat) {
+      case "24h":
+        return day;
+      case "7d":
+        return week;
+      case "1y":
+        return year;
+      default:
+        return day;
+    }
+  };
 
   useEffect(() => {
-    if (chartRef && chartRef.current) {
+    if (chartRef && chartRef.current && detail) {
       const chartInstance = new Chartjs(chartRef.current, {
         type: "line",
         data: {
           datasets: [
             {
-              label: "# of Votes",
-              data: [
-                { x: 1, y: 15 },
-                { x: 2, y: 12 },
-                { x: 3, y: 25 },
-                { x: 4, y: 23 },
-                { x: 5, y: 20 },
-              ],
+              label: `${detail.name} price`,
+              data: determineTimeFormat(),
               backgroundColor: "rgba(174, 305, 194, 0.5)",
               borderColor: "rgba(174, 305, 194, 0.4)",
               pointRadius: "0",
@@ -28,15 +37,40 @@ const HistoryChart = () => {
             },
           ],
         },
-        options: historyOptions,
+        options: { ...historyOptions },
       });
     }
   });
+
+  const renderPrice = () => {
+    if (detail) {
+      return (
+        <>
+          <p className="current-price">&#x20AC;{detail.current_price.toFixed(2)}</p>
+          <p className={detail.price_change_24h < 0 ? "text-red" : "text-green"}>
+            {detail.price_change_24h.toFixed(2)}%
+          </p>
+        </>
+      );
+    }
+  };
+
   return (
     <div className="chart-container">
-      <div></div>
+      <div>{renderPrice()}</div>
       <div>
         <canvas ref={chartRef} id="myChart"></canvas>
+      </div>
+      <div className="button-container">
+        <button onClick={() => setTimeFormat("24h")} className="1d">
+          24h
+        </button>
+        <button onClick={() => setTimeFormat("7d")} className="7d">
+          7d
+        </button>
+        <button onClick={() => setTimeFormat("1y")} className="365d">
+          1y
+        </button>
       </div>
     </div>
   );
