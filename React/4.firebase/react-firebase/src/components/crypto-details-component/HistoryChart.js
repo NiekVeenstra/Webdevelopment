bcbbdf0edbb37866b3f29282e104b71bf22eea60
Niek <n.veenstra02@gmail.com>
2021-04-02 11:40:52 +0200

    add chartjs and setup config

--- React/4.firebase/react-firebase/src/components/crypto-details-component/HistoryChart.js
+++ React/4.firebase/react-firebase/src/components/crypto-details-component/HistoryChart.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
+import { Line, defaults } from "react-chartjs-2";
+import Chartjs from "chart.js";
+import { historyOptions } from "../../chartConfigs/chartConfigs";
 
 const HistoryChart = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const chartRef = useRef();
 
-export default HistoryChart
+  useEffect(() => {
+    if (chartRef && chartRef.current) {
+      const chartInstance = new Chartjs(chartRef.current, {
+        type: "line",
+        data: {
+          datasets: [
+            {
+              label: "# of Votes",
+              data: [
+                { x: 1, y: 15 },
+                { x: 2, y: 12 },
+                { x: 3, y: 25 },
+                { x: 4, y: 23 },
+                { x: 5, y: 20 },
+              ],
+              backgroundColor: "rgba(174, 305, 194, 0.5)",
+              borderColor: "rgba(174, 305, 194, 0.4)",
+              pointRadius: "0",
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: historyOptions,
+      });
+    }
+  });
+  return (
+    <div className="chart-container">
+      <div></div>
+      <div>
+        <canvas ref={chartRef} id="myChart"></canvas>
+      </div>
+    </div>
+  );
+};
+
+export default HistoryChart;
