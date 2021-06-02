ae7f0cab3aeb84bd7c07ab0e39b8ed71cef527ac
Niek <n.veenstra02@gmail.com>
2021-06-02 11:22:46 +0200

    typescript

--- PHP/First PHP Project/functions.php
+++ PHP/First PHP Project/functions.php
@@ -19,5 +19,7 @@ $num01 = $_GET["num01"];
 $oper = $_GET["oper"];
 $num02 = $_GET["num02"];
     
-echo "Value: " . myCalculator($num01, $oper, $num02)
+echo "Value: " . myCalculator($num01, $oper, $num02);
+
+
     
